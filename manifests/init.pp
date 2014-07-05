795bf4f59ed06fd06759be6af86f4df3d6cdd13d
Andrew Marks <amracks@gmail.com>
2014-07-05 11:36:54 -0700

    Created separate webapp host for jenkins

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,41 @@
-include pkgng
 
-class tomcat7 {
+class tomcat7 (
+    $svrconfsrc = "/root/etc/tomcat/server.xml"
+){
+
+    include pkgng
+
+    $tomcat_home = '/usr/local/apache-tomcat-7.0'
 
     package{ 'tomcat7':
         ensure => installed,
     }
 
+    file { 'serverxml':
+        path    => "$tomcat_home/conf/server.xml",
+        ensure  => file,
+        owner   => 'www',
+        group   => 'www',
+        source  => $svrconfsrc,
+        require => Package['tomcat7'],
+        notify  => Service['tomcat7']
+    }
+
+    file { 'jenkappdir':
+        path    => "$tomcat_home/jenkins",
+        ensure  => directory,
+        owner   => 'www',
+        group   => 'www',
+        require => Package['tomcat7'],
+        notify  => Service['nginx']
+    }
+
     service{ 'tomcat7':
         ensure => running,
-        enable => true
+        enable => true,
+        require => [
+            File['serverxml'],
+            File['jenkappdir']
+        ]
     }
 }
